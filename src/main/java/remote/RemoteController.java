c7d8ebab84ad1718a730e8d74ca01cc89f54c0ee
Cyrus Mahdavi <mahdavic2@gmail.com>
2019-08-07 15:49:57 -0500

    Swagger working on all "/remote/*" endpoints

--- src/main/java/remote/RemoteController.java
+++ src/main/java/remote/RemoteController.java
@@ -1,12 +1,14 @@
 package remote;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/remote")
 public class RemoteController {
 
-    @RequestMapping("/")
+    @GetMapping("/hi")
     public String index() {
         return "Whyfff";
     }
