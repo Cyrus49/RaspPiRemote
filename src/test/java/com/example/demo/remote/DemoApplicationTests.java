c52a7261b11a07a3b74157ccce6d01494395c9e5
Cyrus Mahdavi <mahdavic2@gmail.com>
2019-08-07 12:59:48 -0500

    Test framework working

--- src/test/java/com/example/demo/remote/DemoApplicationTests.java
+++ src/test/java/com/example/demo/remote/DemoApplicationTests.java
@@ -1,12 +1,14 @@
-package com.example.demo;
+package com.example.demo.remote;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import remote.DemoApplication;
+import remote.RemoteController;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes= RemoteController.class)
 public class DemoApplicationTests {
 
 	@Test
